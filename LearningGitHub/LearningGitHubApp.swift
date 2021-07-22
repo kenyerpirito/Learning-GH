61257f3598ce6dc6dff9043ca4cc67f6be676950
Kiss Zsolt <zsoltkiss123@gmail.com>
2021-07-22 08:37:18 +0200

    Initial Commit

---
+++ LearningGitHub/LearningGitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGitHubApp.swift
+//  LearningGitHub
+//
+//  Created by Kiss Zsolt on 2021. 07. 22..
+//
+
+import SwiftUI
+
+@main
+struct LearningGitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
